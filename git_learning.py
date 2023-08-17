5106c0bcc5b3db67b9feae3a62ee26533e46a5e6
Adewale Aderoju <adesquare001001@gmail.com>
2023-08-17 04:58:14 -0700

    First Commit

---
+++ git_learning.py
@@ -0,0 +1,5 @@
+def cal():
+    val1 = float(input("enter first value: "))
+    val2= float(input("enter second value: "))
+    print(val1 + val2)
+cal()
---
+++ word.py
@@ -0,0 +1,6 @@
+def articles():
+    article = input("enter your article: ")
+    old_word = input("enter old word you like to change: ")
+    new_word = input("enter new world you like to replace: ")
+    new_article = (old_word, new_word)
+articles() 
